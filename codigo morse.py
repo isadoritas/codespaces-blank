31a54442b1bad1045bd27e7240b05d163db91714
isadoritas <126922417+isadoritas@users.noreply.github.com>
2023-03-03 22:27:56 -0300

    Update codigo morse.py

--- codigo morse.py
+++ codigo morse.py
@@ -12,11 +12,9 @@ MORSE_CODE = {
 }
 
 def convert_to_morse(code):
-    # Check that the input code only contains digits and spaces
     if not all(c.isdigit() or c.isspace() for c in code):
         raise ValueError('Input code contains invalid characters')
 
-    # Replace each digit with its corresponding Morse code
     morse_code = ''.join(MORSE_CODE.get(digit, '') for digit in code)
     return morse_code
 
